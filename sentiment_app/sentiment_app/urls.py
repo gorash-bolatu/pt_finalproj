e2727fcca685b84d59158c4ba4c8a97f2e43a24a
gorash-bolatu <cotwi10@yandex.ru>
2025-11-30 21:46:22 +0800

    homepage fix

--- sentiment_app/sentiment_app/urls.py
+++ sentiment_app/sentiment_app/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from reviews import views as review_views  # <-- correct import
+from reviews import views as review_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # Product list
+    path("", review_views.home, name="home"),   # <-- add this
     path("products/", review_views.product_list, name="product_list"),
-
-    # Recommendation endpoint
     path("recommend/", review_views.recommend_view, name="recommend"),
 ]
